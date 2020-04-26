84495979a4454a12b3afbaaf855a718203dae40a
fastnettx <fastnet.txx@gmail.com>
2020-04-26 23:07:03 +0300

    Deleted

--- mysite/locations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('staticUrl/', views.static, name='staticUrl'),
-    path('<slug:dynamicUrl>/', views.dynamic, name='dynamicUrl')
-]
